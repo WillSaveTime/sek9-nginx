9dd6c304db1beed168ceabed5229240bdedac445
dev0088 <ninjadev999@gmail.com>
2022-06-13 14:45:29 -0400

    Added postgres deployment script.

---
+++ deploy-postgres.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+docker stopsek9-postgres
+
+yes | docker system prune
+
+docker run \
+  --namesek9-postgres \
+  --network=sek9 \
+  --ip 172.20.0.4 \
+  --hostnamesek9_postgres \
+  --restart unless-stopped \
+  -p 5432:5432 \
+  -e POSTGRES_DB=sek9 \
+  -e POSTGRES_USER=postgres \
+  -e POSTGRES_PASSWORD=postgres123qwe!@#QWE \
+  -e PGDATA=/var/lib/postgresql/data/pgdata \
+  -v $PWD/postgres-data:/var/lib/postgresql/data \
+  -d postgres
+
+yes | docker system prune
--- deploy-redis.sh
+++ deploy-redis.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env sh
 
-docker stop aligneursfrancais-redis
+docker stop sek9-redis
 
 yes | docker system prune
 
 docker run \
-  --name aligneursfrancais-redis \
-  --network aligneursfrancais \
+  --name sek9-redis \
+  --network sek9 \
   --ip 172.20.0.10 \
-  --hostname aligneursfrancais_redis \
+  --hostname sek9_redis \
   --restart unless-stopped \
   -v $PWD/redis/conf:/etc/redis \
   -p 6379:6379 \
